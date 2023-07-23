7b6300ece0cf6c1139cb591e275db1cda36333f4
Wegra Lee <wegra.lee@gmail.com>
2023-07-24 08:49:04 +0900

    주석 번역 및 보강

--- ch01/bandit.py
+++ ch01/bandit.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 
 class Bandit:
-    def __init__(self, arms=10):
-        self.rates = np.random.rand(arms)
+    def __init__(self, arms=10):  # arms = 슬롯머신 대수
+        self.rates = np.random.rand(arms  # 슬롯머신 각각의 승률 설정(무작위)
 
     def play(self, arm):
         rate = self.rates[arm]
@@ -16,18 +16,20 @@ class Bandit:
 
 class Agent:
     def __init__(self, epsilon, action_size=10):
-        self.epsilon = epsilon
+        self.epsilon = epsilon  # 무작위로 행동할 확률(탐색 확률)
         self.Qs = np.zeros(action_size)
         self.ns = np.zeros(action_size)
 
+    # 슬롯머신의 가치 추정
     def update(self, action, reward):
         self.ns[action] += 1
         self.Qs[action] += (reward - self.Qs[action]) / self.ns[action]
 
+    # 행동 선택(ε-탐욕 정책)
     def get_action(self):
         if np.random.rand() < self.epsilon:
-            return np.random.randint(0, len(self.Qs))
-        return np.argmax(self.Qs)
+            return np.random.randint(0, len(self.Qs))  # 무작위 행동 선택
+        return np.argmax(self.Qs)  # 탐욕 행동 선택
 
 
 if __name__ == '__main__':
@@ -37,25 +39,27 @@ if __name__ == '__main__':
     bandit = Bandit()
     agent = Agent(epsilon)
     total_reward = 0
-    total_rewards = []
-    rates = []
+    total_rewards = []  # 보상 합
+    rates = []          # 승률
 
     for step in range(steps):
-        action = agent.get_action()
-        reward = bandit.play(action)
-        agent.update(action, reward)
+        action = agent.get_action()   # 행동 선택
+        reward = bandit.play(action)  # 실제로 플레이하고 보상을 받음
+        agent.update(action, reward)  # 행동과 보상을 통해 학습
         total_reward += reward
 
-        total_rewards.append(total_reward)
-        rates.append(total_reward / (step + 1))
+        total_rewards.append(total_reward)       # 현재까지의 보상 합 저장
+        rates.append(total_reward / (step + 1))  # 현재까지의 승률 저장
 
     print(total_reward)
 
+    # 그래프 그리기(보상 총합)
     plt.ylabel('Total reward')
     plt.xlabel('Steps')
     plt.plot(total_rewards)
     plt.show()
 
+    # 그래프 그리기(단계 수와 승률)
     plt.ylabel('Rates')
     plt.xlabel('Steps')
     plt.plot(rates)
