613e457dc8dd8ac19a3e4a29241cc65d5908fb61
Wegra Lee <wegra.lee@gmail.com>
2023-07-24 11:10:19 +0900

    주석 번역

--- ch07/dezero1.py
+++ ch07/dezero1.py
@@ -2,14 +2,14 @@ import numpy as np
 from dezero import Variable
 import dezero.functions as F
 
-# Inner products
+# 벡터의 내적
 a = np.array([1, 2, 3])
 b = np.array([4, 5, 6])
-a, b = Variable(a), Variable(b)  # Optional
+a, b = Variable(a), Variable(b)  # 생략 가능
 c = F.matmul(a, b)
 print(c)
 
-# Matrix product
+# 행렬의 곱
 a = np.array([[1, 2], [3, 4]])
 b = np.array([[5, 6], [7, 8]])
 c = F.matmul(a, b)
