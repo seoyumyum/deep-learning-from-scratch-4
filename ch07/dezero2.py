21f8b7c67ff4fc8fc8ecc5144e26ca7463f33fc0
Wegra Lee <wegra.lee@gmail.com>
2023-07-24 11:12:32 +0900

    주석 보강

--- ch07/dezero2.py
+++ ch07/dezero2.py
@@ -8,16 +8,20 @@ def rosenbrock(x0, x1):
 x0 = Variable(np.array(0.0))
 x1 = Variable(np.array(2.0))
 
-lr = 0.001
-iters = 10000
+iters = 10000  # 반복 횟수
+lr = 0.001     # 학습률
 
-for i in range(iters):
+for i in range(iters):  # 갱신 반복
     y = rosenbrock(x0, x1)
 
+    # 이전 반복에서 더해진 미분 초기화
     x0.cleargrad()
     x1.cleargrad()
+
+    # 미분(역전파)
     y.backward()
 
+    # 변수 갱신
     x0.data -= lr * x0.grad.data
     x1.data -= lr * x1.grad.data
 
