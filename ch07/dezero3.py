563e67a066e848246d9b11d13c804fe71d11a2ec
Wegra Lee <wegra.lee@gmail.com>
2023-07-24 11:17:11 +0900

    주석 번역 및 보강

--- ch07/dezero3.py
+++ ch07/dezero3.py
@@ -3,29 +3,34 @@ import matplotlib.pyplot as plt
 from dezero import Variable
 import dezero.functions as F
 
-# トイ・データセット
+# 토이 데이터셋
 np.random.seed(0)
 x = np.random.rand(100, 1)
 y = 5 + 2 * x + np.random.rand(100, 1)
-x, y = Variable(x), Variable(y)  # 省略可能
+x, y = Variable(x), Variable(y)  # 생략 가능
 
+# 매개변수 정의
 W = Variable(np.zeros((1, 1)))
 b = Variable(np.zeros(1))
 
+# 예측 함수
 def predict(x):
-    y = F.matmul(x, W) + b
+    y = F.matmul(x, W) + b  # 행렬의 곱으로 여러 데이터 일괄 계산
     return y
 
+# 평균 제곱 오차(식 7.2) 계산 함수
 def mean_squared_error(x0, x1):
     diff = x0 - x1
     return F.sum(diff ** 2) / len(diff)
 
+# 경사 하강법으로 매개변수 갱신
 lr = 0.1
 iters = 100
 
 for i in range(iters):
     y_pred = predict(x)
     loss = mean_squared_error(y, y_pred)
+    # 또는 loss = F.mean_squared_error(y, y_pred)
 
     W.cleargrad()
     b.cleargrad()
@@ -34,14 +39,14 @@ for i in range(iters):
     W.data -= lr * W.grad.data
     b.data -= lr * b.grad.data
 
-    if i % 10 == 0:
+    if i % 10 == 0:  # 10회 반복마다 출력
         print(loss.data)
 
 print('====')
 print('W =', W.data)
 print('b =', b.data)
 
-# Plot
+# [그림 7-9] 학습 후 모델
 plt.scatter(x.data, y.data, s=10)
 plt.xlabel('x')
 plt.ylabel('y')
