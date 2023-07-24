d699bec2eb06811b4ea197bc2892a149b88ed25b
Wegra Lee <wegra.lee@gmail.com>
2023-07-24 10:06:11 +0900

    주석 번역 및 보강

--- ch05/importance_sampling.py
+++ ch05/importance_sampling.py
@@ -1,27 +1,29 @@
 import numpy as np
 
-x = np.array([1, 2, 3])
-pi = np.array([0.1, 0.1, 0.8])
+x = np.array([1, 2, 3])         # 확률 변수
+pi = np.array([0.1, 0.1, 0.8])  # 확률 분포
 
-# =========== Expectation ==================
+# =========== 기댓값 ==================
 e = np.sum(x * pi)
 print('E_pi[x]', e)
 
-# =========== Monte Carlo ==================
-n = 100
+# =========== 몬테카를로법 ==================
+n = 100  # 샘플 개수
 samples = []
 for _ in range(n):
-    s = np.random.choice(x, p=pi)
+    s = np.random.choice(x, p=pi)  # pi를 이용한 샘플링
     samples.append(s)
+
 print('MC: {:.2f} (var: {:.2f})'.format(np.mean(samples), np.var(samples)))
 
-# =========== Importance Sampling ===========
+# =========== 중요도 샘플링 ===========
 b = np.array([0.2, 0.2, 0.6])  #b = np.array([1/3, 1/3, 1/3])
 samples = []
 for _ in range(n):
-    idx = np.arange(len(b))  # [0, 1, 2]
-    i = np.random.choice(idx, p=b)
+    idx = np.arange(len(b))         # b의 인덱스([0, 1, 2])
+    i = np.random.choice(idx, p=b)  # b를 사용하여 샘플링
     s = x[i]
-    rho = pi[i] / b[i]
-    samples.append(rho * s)
+    rho = pi[i] / b[i]              # 가중치
+    samples.append(rho * s)         # 샘플 데이터에 가중치를 곱해 저장
+    
 print('IS: {:.2f} (var: {:.2f})'.format(np.mean(samples), np.var(samples)))
