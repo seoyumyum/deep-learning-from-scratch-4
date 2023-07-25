f5af335c0965d435d2b2542026a48c0c2ed054c2
Wegra Lee <wegra.lee@gmail.com>
2023-07-25 21:29:18 +0900

    주석 보강

--- ch09/actor_critic.py
+++ ch09/actor_critic.py
@@ -9,7 +9,7 @@ import dezero.functions as F
 import dezero.layers as L
 
 
-class PolicyNet(Model):
+class PolicyNet(Model):  # 정책 신경망
     def __init__(self, action_size=2):
         super().__init__()
         self.l1 = L.Linear(128)
@@ -18,11 +18,11 @@ class PolicyNet(Model):
     def forward(self, x):
         x = F.relu(self.l1(x))
         x = self.l2(x)
-        x = F.softmax(x)
+        x = F.softmax(x)  # 확률 출력
         return x
 
 
-class ValueNet(Model):
+class ValueNet(Model):  # 가치 함수 신경망
     def __init__(self):
         super().__init__()
         self.l1 = L.Linear(128)
@@ -47,27 +47,29 @@ class Agent:
         self.optimizer_v = optimizers.Adam(self.lr_v).setup(self.v)
 
     def get_action(self, state):
-        state = state[np.newaxis, :]  # add batch axis
+        state = state[np.newaxis, :]  # 배치 처리용 축 추가
         probs = self.pi(state)
         probs = probs[0]
         action = np.random.choice(len(probs), p=probs.data)
-        return action, probs[action]
+        return action, probs[action]  # 선택된 행동과 해당 행동의 확률 반환
 
     def update(self, state, action_prob, reward, next_state, done):
-        state = state[np.newaxis, :]  # add batch axis
+        # 배치 처리용 축 추가
+        state = state[np.newaxis, :]
         next_state = next_state[np.newaxis, :]
 
-        # ========== (1) Update V network ===========
-        target = reward + self.gamma * self.v(next_state) * (1 - done)
+        # 가치 함수(self.v)의 손실 계산
+        target = reward + self.gamma * self.v(next_state) * (1 - done)  # TD 목표
         target.unchain()
-        v = self.v(state)
-        loss_v = F.mean_squared_error(v, target)
+        v = self.v(state)  # 현재 상태의 가치 함수
+        loss_v = F.mean_squared_error(v, target)  # 두 값의 평균 제곱 오차
 
-        # ========== (2) Update pi network ===========
+        # 정책(self.pi)의 손실 계산
         delta = target - v
         delta.unchain()
         loss_pi = -F.log(action_prob) * delta
 
+        # 신경망 학습
         self.v.cleargrads()
         self.pi.cleargrads()
         loss_v.backward()
@@ -100,6 +102,6 @@ for episode in range(episodes):
         print("episode :{}, total reward : {:.1f}".format(episode, total_reward))
 
 
-# plot
+# [그림 9-11]의 왼쪽 그래프
 from common.utils import plot_total_reward
 plot_total_reward(reward_history)
