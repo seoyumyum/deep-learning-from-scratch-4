1832c2c22eef2287bb3c748d31e8ec5a90085b06
Wegra Lee <wegra.lee@gmail.com>
2023-07-25 21:06:13 +0900

    Update gym_play.py

--- ch08/gym_play.py
+++ ch08/gym_play.py
@@ -6,8 +6,8 @@ env = gym.make('CartPole-v0')
 state = env.reset()
 done = False
 
-while not done:
-    env.render()
-    action = np.random.choice([0, 1])
+while not done:  # 에피소드가 끝날 때까지 반복
+    env.render()  # 진행 과정 시각화
+    action = np.random.choice([0, 1])  # 행동 선택(무작위)
     next_state, reward, done, info = env.step(action)
 env.close()
