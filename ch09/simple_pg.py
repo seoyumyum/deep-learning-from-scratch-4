f24d5c4adbe0e6f09271600aa446453d293679ad
Wegra Lee <wegra.lee@gmail.com>
2023-07-25 21:21:59 +0900

    주석 보강

--- ch09/simple_pg.py
+++ ch09/simple_pg.py
@@ -12,12 +12,12 @@ import dezero.layers as L
 class Policy(Model):
     def __init__(self, action_size):
         super().__init__()
-        self.l1 = L.Linear(128)
-        self.l2 = L.Linear(action_size)
+        self.l1 = L.Linear(128)          # 첫 번째 계층
+        self.l2 = L.Linear(action_size)  # 두 번째 계층
 
     def forward(self, x):
-        x = F.relu(self.l1(x))
-        x = F.softmax(self.l2(x))
+        x = F.relu(self.l1(x))     # 첫 번째 계층에서는 ReLU 함수 사용
+        x = F.softmax(self.l2(x))  # 두 번째 계층에서는 소프트맥스 함수 사용
         return x
 
 
@@ -33,11 +33,11 @@ class Agent:
         self.optimizer.setup(self.pi)
 
     def get_action(self, state):
-        state = state[np.newaxis, :]
-        probs = self.pi(state)
+        state = state[np.newaxis, :]  # 배치 처리용 축 추가
+        probs = self.pi(state)        # 순전파 수행
         probs = probs[0]
-        action = np.random.choice(len(probs), p=probs.data)
-        return action, probs[action]
+        action = np.random.choice(len(probs), p=probs.data)  # 행동 선택
+        return action, probs[action]  # 선택된 행동과 확률 반환
 
     def add(self, reward, prob):
         data = (reward, prob)
@@ -47,15 +47,15 @@ class Agent:
         self.pi.cleargrads()
 
         G, loss = 0, 0
-        for reward, prob in reversed(self.memory):
+        for reward, prob in reversed(self.memory):  # 수익 G 계산
             G = reward + self.gamma * G
 
-        for reward, prob in self.memory:
+        for reward, prob in self.memory:  # 손실 함수 계산
             loss += -F.log(prob) * G
 
         loss.backward()
         self.optimizer.update()
-        self.memory = []
+        self.memory = []  # 메모리 초기화
 
 
 episodes = 3000
@@ -69,14 +69,14 @@ for episode in range(episodes):
     total_reward = 0
 
     while not done:
-        action, prob = agent.get_action(state)
-        next_state, reward, done, info = env.step(action)
+        action, prob = agent.get_action(state)  # 행동 선택
+        next_state, reward, done, info = env.step(action)  # 행동 수행
 
-        agent.add(reward, prob)
-        state = next_state
-        total_reward += reward
+        agent.add(reward, prob)  # 보상과 행동의 확률을 에이전트에 추가
+        state = next_state       # 상태 전이
+        total_reward += reward   # 보상 총합 계산
 
-    agent.update()
+    agent.update()  # 정책 갱신
 
     reward_history.append(total_reward)
     if episode % 100 == 0:
