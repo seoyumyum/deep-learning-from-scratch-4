74cf3fcdfdfd23574daba32de5b1ec75e4fce0a5
Austin <seoyumyum@gmail.com>
2024-09-23 09:53:16 -0700

    N/A

--- ch04/gridworld_play.py
+++ ch04/gridworld_play.py
@@ -7,5 +7,5 @@ from common.gridworld import GridWorld
 env = GridWorld()
 V = {}
 for state in env.states():
-    V[state] = np.random.randn()  # 더미 상태 가치 함수
+    V[state] = np.random.randn()  # 더미 상태 가치 함수 .
 env.render_v(V)
